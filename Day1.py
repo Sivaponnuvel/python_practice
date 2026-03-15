a20caf5fe762f0af69ea5471d2bd0dcb3195ee8b
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-03-15 12:17:00 +0530

    Day1 frist question

---
+++ Day1.py
@@ -0,0 +1,9 @@
+# Question 1
+# Write a Python program to display your name, age, and city using variables.
+
+name = input("enter the name :")
+age = int(input("enter the age:"))
+city = input("enter the city:")
+print(f"my name is {name}")
+print(f"my age is {age}")
+print(f"my city is {city}")
