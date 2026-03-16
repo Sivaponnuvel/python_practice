15fd622d0685a6ba5b288be862f0bb19a2eb2eb0
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-03-16 09:17:50 +0530

    Day2 second question

--- Day2.py
+++ Day2.py
@@ -3,3 +3,11 @@
 
 a = int(input("enter the number : "))
 print(a**2)
+
+
+# Question 2
+# Write a Python program to take two numbers from the user and print their multiplication.
+
+s = int(input("enter the 1st number : "))
+p = int(input("enter the 2nd number : "))
+print(f"multiplication number is {s*p}")
