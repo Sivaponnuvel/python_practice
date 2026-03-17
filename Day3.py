031bb9a5a45172d1208b636d38f273f83c497c51
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-03-17 09:09:46 +0530

    Day3 first question

---
+++ Day3.py
@@ -0,0 +1,9 @@
+# Question 1
+# Write a Python program to take a number from the user and check whether it is even or odd.
+
+a = int(input("enter the number : "))
+if a % 2 == 0:
+    print(f"{a} is even number")
+else:
+    print(f"{a} is odd number")
+
