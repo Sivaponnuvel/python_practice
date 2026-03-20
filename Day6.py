117e5d361dcdf1c349bc5555156362f47fb23632
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-03-20 06:59:36 +0530

    Day6 second question

--- Day6.py
+++ Day6.py
@@ -6,3 +6,13 @@ def num(n):
         print(i+1)
 num(10)
 
+
+# Question 2
+# Write a Python program to take a number from the user and print its multiplication table (1 to 10).
+
+def num(x):
+    for i in range(1,11):
+        ans = i * x
+        print(f"{i} X {x} = {ans}")
+a = int(input("enter the number : "))
+num(a)
