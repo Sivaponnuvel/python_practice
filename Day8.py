6d4e961d8ee853ae6ac5d67e2afce769a861c45f
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-03-22 19:21:55 +0530

    Day8 second question

--- Day8.py
+++ Day8.py
@@ -15,3 +15,18 @@ a = int(input("enter the number : "))
 star(a)
 
 
+# Question 2
+# Write a Python program to print this pattern:
+# 1
+# 12
+# 123
+# 1234
+# 12345
+
+def num(s):
+    for i in range(1,s+1):
+        for j in range(1,i+1):
+            print(j , end="")
+        print()
+x = int(input("enter the number : "))
+num(x)
