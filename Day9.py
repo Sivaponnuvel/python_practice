d97f2b0d31b0ba433843cbf6cacda9994753d2c7
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-03-23 08:57:54 +0530

    Day9 second question use built-in funcation

--- Day9.py
+++ Day9.py
@@ -10,3 +10,16 @@ n = input("Enter the name : ")
 vow(n)
 
 
+# Question 2
+# Write a Python program to take a string from the user and check whether it is a palindrome or not.
+
+# use built-in function 
+def palin(n):
+    a = n[::-1]
+    if n == a:
+        print(f"{n} is palindrome")
+    else:
+        print(f"{n} is not a palindrome")
+s =  input("Enter the name : ")
+palin(s)   
+
