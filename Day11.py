8ac5c4da392f3023fb288cac3c59ca776b446266
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-03-25 10:53:03 +0530

    Day11 second question

--- Day11.py
+++ Day11.py
@@ -12,3 +12,15 @@ x = list(map(int,x))
 even(x)
 
 
+# Question 2
+# Write a Python program to take a list of numbers from the user and count how many numbers are greater than 10.
+
+def count_greater(s):
+    num = 0
+    for i in s:
+        if i > 10:
+           num += 1
+    print(f"Count of numbers greater than 10: {num}") 
+a = input("Enter the numbers : ").split()
+a = list(map(int,a))
+count_greater(a)
