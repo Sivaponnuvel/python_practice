0bac24d3cec8daa270571d8fe5f1107893a0af80
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-03-30 11:52:41 +0530

    Day16 second question

--- Day16.py
+++ Day16.py
@@ -20,3 +20,27 @@ s = list(map(int,s))
 prime_number(s)
 
 
+# Question 2
+# Write a Python program to:
+# 👉 Take a string from the user
+# 👉 Find the most frequent character in the string
+# Example:
+# Input: aabbccc
+# Output: c
+
+def char(a):
+    freq = {}
+    for i in a:
+        if i in freq:
+            freq[i] += 1
+        else:
+            freq[i] = 1
+    max_char = ''
+    max_count = 0
+    for i in freq:
+        if freq[i] > max_count:
+            max_count = freq[i]
+            max_char = i
+    print(max_char)
+string = input("enter the string : ")
+char(string)
