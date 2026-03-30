5fb61e33959b51fc2b1d6ef253875c7bea2f080d
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-03-30 11:51:39 +0530

    Day16 first question

---
+++ Day16.py
@@ -0,0 +1,22 @@
+# Question 1
+# Write a Python program to:
+# 👉 Take a list of numbers from the user
+# 👉 Print all prime numbers in the list
+
+def prime_number(a):
+    prime_numbers = []
+    for i in a:
+        if i > 1:
+            is_prime  = True
+            for j in range(2,i):
+                if i % j == 0:
+                    is_prime = False
+                    break
+            if is_prime:
+                prime_numbers.append(i)
+    print(prime_numbers)
+s = input("enter the numbers : ").split()
+s = list(map(int,s))
+prime_number(s)
+
+
