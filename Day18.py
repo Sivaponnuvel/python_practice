4f9bd7467e49b93a4d088a05bf32675accdaaa26
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-04-01 11:39:24 +0530

    Day18 first question

---
+++ Day18.py
@@ -0,0 +1,24 @@
+# Question 1
+# Write a Python program to:
+# 👉 Take a list of numbers from the user
+# 👉 Find all pairs of numbers whose sum is equal to a given target
+# 👉 Example:
+# Input list: 1 2 3 4 5
+# Target: 6
+# Output:
+# (1,5)
+# (2,4)
+
+def equal(a,b):
+    s = []
+    for i in a:
+        diff = b-i
+        if diff in s:
+            return diff , i
+        s.append(i)
+user = input("enter the numbers : ").split()
+user = list(map(int,user))
+target = int(input("enter the target number : "))
+print(equal(user,target)) 
+
+
