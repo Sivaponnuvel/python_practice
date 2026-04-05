48b50d865d90e5ca7f1622c0c8f61c5198c298e5
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-04-05 09:54:31 +0530

    Day22 second question

--- Day22.py
+++ Day22.py
@@ -31,3 +31,39 @@ print(f"Area : {s.area()}")
 print(f"Perimeter : {s.perimeter()}")
 
 
+# 🎯 Question 2
+# Write a Python program to:
+# 👉 Create a class Employee
+# 👉 Private variables:
+# __name
+# __salary
+# 👉 Methods:
+# set_details(name, salary)
+# get_details()
+# increase_salary(percent)
+# 👉 Increase salary based on percentage
+# 🧠 Example
+# Name: Siva
+# Salary: 10000
+# After 10% increase → 11000
+
+class Employee:
+    def __init__(self):
+        self.__name = None
+        self.__salary = None
+    def set_details(self,name,salary):
+        self.__name = name
+        self.__salary = salary
+    def get_details(self):
+        print(f"Name : {self.__name} \nSalary : {self.__salary}")
+    def increase_salary(self,percentage):
+        a = self.__salary * percentage/100
+        self.__salary += a
+        print(f"After {percentage}% increase -> {self.__salary}")
+name = input("Enter the employee name : ")
+salary = int(input("Enter the employee salary : "))
+percentage = int(input("Enter the increase_salary : "))
+s1 = Employee()
+s1.set_details(name,salary)
+s1.get_details()
+s1.increase_salary(percentage)
