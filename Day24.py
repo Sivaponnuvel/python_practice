40b1f6adf9fcf1ecb537b809bc9415c1b5e47aff
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-04-07 08:07:19 +0530

    Day24 second question

--- Day24.py
+++ Day24.py
@@ -27,3 +27,30 @@ s.show_mother()
 s.show_child()
 
 
+# 🔥 Question 2 – Multilevel Inheritance
+# Write a Python program to:
+# 👉 Create class Grandparent
+# method: show_grandparent() → print "This is grandparent"
+# 👉 Create class Parent (inherits Grandparent)
+# method: show_parent() → print "This is parent"
+# 👉 Create class Child (inherits Parent)
+# method: show_child() → print "This is child"
+# 👉 Create object of Child and call all methods
+# 🧠 Example Output
+# This is grandparent
+# This is parent
+# This is child
+
+class Grandparent:
+    def show_grandparent(self):
+        print("This is grandparent")
+class Parent(Grandparent):
+    def show_parent(self):
+        print("This is parent")
+class Child(Parent):
+    def show_child(self):
+        print("This is child")
+s1 = Child()
+s1.show_grandparent()
+s1.show_parent()
+s1.show_child()
