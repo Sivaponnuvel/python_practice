e1481186712b3de714e5a32d4b2ba7bed628a258
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-04-08 09:12:33 +0530

    Day25 second question

--- Day25.py
+++ Day25.py
@@ -19,3 +19,29 @@ obj = Dog()
 print(f"{obj.sound()}")
 
 
+# 🔥 Question 2 - Method Overriding
+# Write a Python program to:
+# 👉 Create class Shape
+# method: area() → print "Calculating area"
+# 👉 Create class Circle (inherits Shape)
+# override method area() → print "Area of circle"
+# 👉 Create class Rectangle (inherits Shape)
+# override method area() → print "Area of rectangle"
+# 👉 Create objects for Circle and Rectangle and call methods
+# 🧠 Example Output
+# Area of circle
+# Area of rectangle
+
+class Shape:
+    def area(self):
+        return "Calculating area"
+class Circle(Shape):
+    def area(self):
+        return "Area of circle"
+class Rectangle(Shape):
+    def area(self):
+        return "Area of rectangle"
+obj1 = Circle()
+obj2 = Rectangle()
+print(f"{obj1.area()}")
+print(f"{obj2.area()}")
