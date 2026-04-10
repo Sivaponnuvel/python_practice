88fe6328d7cf22bfae9ba816aa66b0bc25b799da
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-04-10 08:49:23 +0530

    Day27 second question

--- Day27.py
+++ Day27.py
@@ -13,3 +13,17 @@ name = input("Enter your name : ")
 greet(name)
 
 
+# 🔥 Question 2
+# Write a Python program to:
+# 👉 Create a function add_numbers()
+# take two numbers as parameters
+# return their sum
+# 👉 Call the function and print result
+# 🧠 Example Output
+# Sum: 30
+
+def add_numbers(num1,num2):
+    return num1 + num2
+num1 = int(input("Enter the first number : "))
+num2 = int(input("Enter the second number : "))
+print(f"Sum: {add_numbers(num1,num2)}")
