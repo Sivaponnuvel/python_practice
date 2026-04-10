efd412373ae2acb21c93f03e6eb76d6993a28b58
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-04-10 08:48:42 +0530

    Day27 first question

---
+++ Day27.py
@@ -0,0 +1,15 @@
+# 🔥 Question 1
+# Write a Python program to:
+# 👉 Create a function greet()
+# take name as parameter
+# print "Hello <name>"
+# 👉 Call the function
+# 🧠 Example Output
+# Hello Siva
+
+def greet(name):
+    print(f"Hello {name}")
+name = input("Enter your name : ")
+greet(name)
+
+
