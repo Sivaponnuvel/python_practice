a7ef85a9f49a526306629b6f6aedf79a7ae07109
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-04-12 00:30:26 +0530

    Day29 first question

---
+++ Day29.py
@@ -0,0 +1,14 @@
+# 🔥 Question 1
+# Write a Python program to:
+# 👉 Create a lambda function to add two numbers
+# 👉 Take two numbers from user
+# 👉 Print the result
+# 🧠 Example Output
+# Sum: 30
+
+x = lambda a,b : a+b
+a = int(input("Enter the number : "))
+b = int(input("Enter the number : "))
+print(f"Sum: {x(a,b)}")
+
+
