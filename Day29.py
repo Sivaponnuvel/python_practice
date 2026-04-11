d0f14968d3c4d1cf99e8f398bc1fecdb26b9ad4e
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-04-12 00:32:15 +0530

    Day29 second question

--- Day29.py
+++ Day29.py
@@ -12,3 +12,16 @@ b = int(input("Enter the number : "))
 print(f"Sum: {x(a,b)}")
 
 
+# 🔥 Question 2
+# Write a Python program to:
+# 👉 Take a list of numbers from user
+# 👉 Use lambda with map()
+# to square each number
+# 👉 Print the result as a list
+# 🧠 Example Output
+# Input: 1 2 3 4
+# Output: [1, 4, 9, 16]
+
+s = lambda a : list(map(lambda x : x**2,a))
+a = list(map(int,input("Enter the numbers : ").split()))
+print(s(a))
