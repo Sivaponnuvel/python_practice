94fa9ae41de7abe4f6b46d00a1a7f7ef2eca8ef7
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-04-14 10:22:22 +0530

    Day31 second question

--- Day31/main.py
+++ Day31/main.py
@@ -17,3 +17,29 @@ print(file.read())
 file.close()
 
 
+# 🔥 Question 2
+# Write a Python program to:
+# 👉 Take a sentence from user
+# 👉 Write it into a file sentence.txt
+# 👉 Read the file and count number of words
+# 👉 Print total word count
+# 🧠 Example Output
+# Total words: 5
+
+file1 = open("D:/Python/Own try/practice/Day31/sentence.txt","w")
+sentence = input("Enter the sentence : ")
+file1.write(sentence)
+file1.close()
+
+file1 = open("D:/Python/Own try/practice/Day31/sentence.txt")
+read = file1.read()
+count = 0
+words = False
+for i in read:
+    if i != " " and words == False:
+        count += 1
+        words = True
+    elif i == " ":
+        words = False
+print(f"Total words: {count}")
+file1.close()
