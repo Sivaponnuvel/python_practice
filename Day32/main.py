76936f8a9d0347b8f99ad417b56d1f9e802644a6
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-04-15 12:46:12 +0530

    Day32 second question

--- Day32/main.py
+++ Day32/main.py
@@ -19,3 +19,28 @@ with open("D:/Python/Own try/practice/Day32/log.txt") as file:
     file.close()
 
 
+# 🔥 Question 2
+# Write a Python program to:
+# 👉 Create a file numbers.txt
+# 👉 Take numbers from user (space separated)
+# 👉 Write numbers into file
+# 👉 Read the file and find:
+# total numbers count
+# sum of numbers
+# 🧠 Example Output
+# Count: 5
+# Sum: 50
+
+with open("D:/Python/Own try/practice/Day32/numbers.txt","w") as file:
+    user = list(map(int,input("Enter numbers separated by space : " ).split()))
+    file.write(" ".join(map(str,user)))
+    file.close()
+
+with open("D:/Python/Own try/practice/Day32/numbers.txt") as file:
+    read = file.read().split()
+    numbers = list(map(int,read))
+    count = len(numbers)
+    total = sum(numbers)
+    print(f"Count: {count}")
+    print(f"Sum: {total}")
+    file.close()
