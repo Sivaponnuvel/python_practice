ea2753d1ee0b03d2e478a6d3f75457d4c7de7a19
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-04-20 09:04:08 +0530

    Day37 second question

--- Day37.py
+++ Day37.py
@@ -19,3 +19,23 @@ except ValueError as e:
     print(f"ValueError: {e}")
 
 
+# 🔥 Question 2
+# Write a Python program to:
+# 👉 Take a number from user
+# 👉 If number is less than 10 →
+# raise error: "Number must be >= 10"
+# 👉 Otherwise print square of number
+# 👉 Handle the error using try-except
+# 🧠 Example Output
+# Square: 100
+# OR
+# Error: Number must be >= 10
+
+try:
+    number = int(input("Enter the number : "))
+    if number < 10:
+        raise ValueError("Number must be >= 10")
+except ValueError as e:
+    print(f"Error: {e}")
+else:
+    print(f"Square: {number**2}")
