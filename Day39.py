3a1438f7d50caba4a46e7f1d0fd1d57ba138d808
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-04-22 07:16:46 +0530

    Day39 second question

--- Day39.py
+++ Day39.py
@@ -24,3 +24,32 @@ while True:
         break
 
 
+# 🔥 Question 2
+# Write a Python program to:
+# 👉 Take password from user
+# 👉 Conditions:
+# Must be at least 6 characters
+# Must contain at least one number
+# 👉 Give user only 3 attempts
+# 👉 If all 3 attempts fail → print:
+# "Account locked"
+# 👉 If correct → print:
+# "Access granted"
+
+attempts = 3
+while attempts > 0:
+    try:
+        password = input("Enter your password: ")
+        if len(password) < 6:
+            raise ValueError("Password too short")
+        if not any(i.isdigit() for i in password):
+            raise ValueError("Password  must contain a number")
+    except ValueError as e:
+        attempts -= 1
+        print(f"Error: {e}")
+        print(f"Attempts left: {attempts}\n")
+    else:
+        print("Access granted ✅")
+        break
+if attempts == 0:
+    print("Account locked ❌")
