5553bbb566b7f3aa52977add0825cd66c4b53e19
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-04-23 16:38:22 +0530

    Day40 second question

--- Day40.py
+++ Day40.py
@@ -24,3 +24,37 @@ except ValueError as e:
     print(e)
 
 
+# 🔥 Question 2
+# Write a Python program to:
+# 👉 Create a function validate_password(password)
+# 👉 Conditions:
+# Must be at least 6 characters
+# Must contain at least one number
+# 👉 If invalid → raise error
+# 👉 If valid → return True
+# 👉 Give user 3 attempts
+# 👉 Use loop + try-except
+# 👉 If valid → print:
+# "Access granted"
+# 👉 If 3 attempts fail → print:
+# "Account locked"
+
+def validate_password(password):
+    if len(password) < 6:
+        raise ValueError("Password too short")
+    if not any(i.isdigit() for i in password):
+        raise ValueError("Password must contain a number")
+    return True
+attempts = 3
+while attempts > 0:
+    password = input("Enter password : ")
+    try:
+        if validate_password(password):
+            print("Access granted ✅")
+            break
+    except ValueError as e:
+        attempts -= 1
+        print(f"Error: {e}")
+        print(f"Attempts left: {attempts}\n")
+if attempts == 0:
+    print("Account locked ❌")
