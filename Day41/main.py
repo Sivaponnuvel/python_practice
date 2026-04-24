558dba07bc5326a14e0f55aebe54e7fd712cab84
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-04-24 15:18:50 +0530

    Day41 second question

--- Day41/main.py
+++ Day41/main.py
@@ -28,3 +28,31 @@ else:
     print(f"Division: {m.divide(number1,number2)}")
 
 
+# 🔹 Question 2
+# Write a Python program to:
+# 👉 Use built-in module random
+# 👉 Generate a random number between 1 to 10
+# 👉 Ask user to guess the number
+# 👉 If correct → print "Correct guess"
+# 👉 If wrong → print "Wrong guess" + show correct number
+# 🧠 Example Output:
+# Enter your guess: 5
+# Wrong guess ❌
+# Correct number was: 7
+
+import random
+attempts = 5
+correct_number = random.randint(1,10)
+while attempts > 0:
+    try:
+        user = int(input("Enter your guess number (1-10): "))
+        if user == correct_number:
+            print("Correct guess ✅")
+            break
+        else:
+            attempts -= 1
+            print(f"Wrong Guess❌\nAttempts left: {attempts}")
+    except ValueError:
+        print("Invalid input ❌")
+if attempts == 0:
+    print(f"Game over ❌ Correct number was: {correct_number}")
