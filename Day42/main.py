d9ffb5305ed3fc948a4ba2702663c073a3622214
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-04-25 13:10:23 +0530

    Day42 second question

--- Day42/main.py
+++ Day42/main.py
@@ -27,3 +27,28 @@ else:
     print(f"Subtraction: {mymath.subtract(num1,num2)}")
 
 
+# 🔹 Question 2
+# Write a Python program to:
+# 👉 Create a package named utilities
+# 👉 Inside it create two modules:
+# string_utils.py → function: to_upper(text)
+# number_utils.py → function: is_even(num)
+# 👉 In main.py:
+# Import both modules from package
+# Take user input (string + number)
+# Call both functions
+# Print results
+# 🧠 Example Output:
+# Uppercase: HELLO
+# Even Number: True
+
+from utilities import number_utils
+from utilities import string_utils
+try:
+    string = input("Enter a string: ")
+    number = int(input("Enter the number: "))
+except ValueError:
+    print("Invalid input ❌")
+else:
+    print(f"Uppercase: {string_utils.to_upper(string)}")
+    print(f"Even Number: {number_utils.is_even(number)}")
