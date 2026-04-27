cba852b9515a9dfbadea3996b673e497e94fc95a
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-04-28 04:23:38 +0530

    Day45 first question

---
+++ Day45.py
@@ -0,0 +1,14 @@
+# 🔹 Question 1
+# 👉 Create a dictionary with:
+# name
+# age
+# city
+# 👉 Convert it to JSON
+# 👉 Print it
+
+import json
+person = {'name':'Siva','age': 23,'city':'Chennai'}
+a = json.dumps(person)
+print(a)
+
+
