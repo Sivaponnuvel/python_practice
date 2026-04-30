f66e04b9ab0404fb77e52db074d290055eab2ff0
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-04-30 23:09:26 +0530

    Day46 second question

--- Day46/main.py
+++ Day46/main.py
@@ -18,3 +18,25 @@ with open("D:/Python/Own try/practice/Day46/data.json", "r") as file:
 print(read)
 
 
+# 🔹 Question 2
+# Write a Python program to:
+# 👉 Take input from user:
+# name
+# age
+# 👉 Store it in user.json using JSON
+# 👉 Read the file
+# 👉 Print in format:
+# 🧠 Example Output:
+# Name: Siva
+# Age: 23
+
+import json
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+x = {'name': name, 'age': age}
+with open("D:/Python/Own try/practice/Day46/user.json","w") as file:
+    json.dump(x, file)
+with open("D:/Python/Own try/practice/Day46/user.json","r") as file:
+    a = json.load(file)
+print(f"Name: {a['name']}")
+print(f"Age: {a['age']}")
