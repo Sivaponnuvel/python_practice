930fe5d5b0eef5d966151ec1080fffff7bef7bae
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-05-02 12:11:37 +0530

    Day48 second question 1st method

--- Day48.py
+++ Day48.py
@@ -36,3 +36,42 @@ else:
     print(f"Max: {find_max(numbers)}")
     print(f"Min: {find_min(numbers)}")
     print(f"Sum: {find_sum(numbers)}")
+
+
+# 🔹 Question 2 – String Validation (Real-world)
+# Write a Python program to:
+# 👉 Take a string from user
+# 👉 Count:
+# vowels
+# consonants
+# digits
+# 👉 Print result
+# 🧠 Example:
+# Input: Siva123
+# Vowels: 2
+# Consonants: 2
+# Digits: 3
+
+# method-1
+def vowels(a):
+    count_vowels = 0
+    for i in a:
+        if i in "aeiouAEIOU":
+            count_vowels += 1
+    return count_vowels
+def consonants(b):
+    count_consonants = 0
+    for i in b:
+        if i.isalpha() and i not in "aeiouAEIOU":
+            count_consonants += 1
+    return count_consonants
+def digits(c):
+    count_digits = 0
+    for i in c:
+        if i.isdigit():
+            count_digits += 1
+    return count_digits
+user = input("Enter string: ")
+print(f"Vowels: {vowels(user)}")
+print(f"Consonants: {consonants(user)}")
+print(f"Digits: {digits(user)}")
