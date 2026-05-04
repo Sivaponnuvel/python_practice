ce3f78bdd690d6c2ae80abc21ef9fc74856eb147
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-05-04 12:48:41 +0530

    Day50 second question

--- Day50.py
+++ Day50.py
@@ -35,3 +35,43 @@ for key, value in person.items():
     print(f"{key} : {value}")
 
 
+# 🔹 Question 2 – Dictionary with Multiple Students
+# Write a Python program to:
+# 👉 Create a dictionary:
+# students = {
+#     "Siva": 85,
+#     "Ram": 70,
+#     "Arun": 90
+# }
+# 👉 Do the following:
+# Print all students with marks
+# Count how many students
+# Find student with marks > 80
+# 🧠 Example Output:
+# All Students:
+# Siva - 85
+# Ram - 70
+# Arun - 90
+# Total Students: 3
+# Above 80:
+# Siva - 85
+# Arun - 90
+
+students = {}
+n = int(input("How many students? "))
+for i in range(n):
+    name = input("Enter student name: ")
+    mark = int(input("Enter mark: "))
+    students[name] = mark
+print("All Students:")
+for key , value in students.items():
+    print(f"{key} - {value}")
+print(f"Total Students: {len(students)}")
+print("Above 80:")
+found = False
+for key , value in students.items():
+    if value > 80:
+        print(f"{key} - {value}")
+        found = True
+if not found:
+    print("No students above 80")
