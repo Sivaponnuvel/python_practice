47a87d0651ffdad277e830087e95a1722ad8d15f
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-05-05 12:51:12 +0530

    Day51 second question

--- Day51.py
+++ Day51.py
@@ -43,3 +43,30 @@ for value in students.values():
     print(value["marks"])
 
 
+# 🔹 Question 2 – Filter Students
+# 👉 Using the same nested dictionary:
+# Print students with marks > 80
+# Find and print topper (highest marks)
+# 🧠 Example Output:
+# Above 80:
+# Siva - 85
+# Ram - 90
+# Topper:
+# Ram - 90
+
+print("Above 80:")
+found = False
+for key, value in students.items():
+    if value["marks"] > 80:
+        print(f"{key} - {value['marks']}")
+        found = True
+if not found:
+    print("No students above 80")
+print("Topper:")
+highest_mark = 0
+topper_name = ""
+for key, value in students.items():
+    if value["marks"] > highest_mark:
+        highest_mark = value["marks"]
+        topper_name = key
+print(f"{topper_name} - {highest_mark}")
