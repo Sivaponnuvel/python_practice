1bbb5bcdf5b9b29d1be890e5fb443fe26118951c
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-05-10 17:06:36 +0530

    Day56 second question

--- Day56.py
+++ Day56.py
@@ -22,3 +22,28 @@ print(f"Name: {user['name']}")
 print(f"Age: {user['age']}")
 
 
+# 🔹 Question 2 – List of Users
+# Write a Python program to:
+# 👉 Create an empty list:
+# users = []
+# 👉 Take details for 3 users:
+# name
+# age
+# 👉 Store each user as dictionary inside list
+# Example:
+# [
+#     {"name": "Siva", "age": 23},
+#     {"name": "Ram", "age": 21}
+# ]
+# 👉 Print all users like:
+# Siva - 23
+# Ram - 21
+
+users = []
+for i in range(3):
+    name = input("Enter your name: ")
+    age = int(input("Enter your age: "))
+    user = {"name": name, "age": age}
+    users.append(user)
+for user in users:
+    print(f"{user['name']} - {user['age']}")
