43f7900bbde011cef3282ce374f18b1465087cfd
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-05-12 00:16:19 +0530

    Day58 second question

--- Day58.py
+++ Day58.py
@@ -31,3 +31,40 @@ print(f"Name: {user['name']}")
 print(f"Age: {user['details']['age']}")
 print(f"Marks: {user['details']['marks']}")
 
+
+# 🔹 Question 2 – User Login Validation
+# Write a Python program to:
+# 👉 Create a dictionary:
+# user_data = {
+#     "username": "admin",
+#     "password": "1234"
+# }
+# 👉 Take username and password from user
+# Rules:
+# username should match stored username
+# password should match stored password
+# 👉 If invalid:
+# raise ValueError
+# 👉 Otherwise print:
+# Login Successful ✅
+# 👉 Handle errors using try-except
+# Example Output:
+# Enter username: admin
+# Enter password: 1234
+# Login Successful ✅
+# OR
+# Error: Invalid username or password ❌
+
+user_data = {
+    "username": "admin",
+    "password": "1234"
+}
+username = input("Enter your username: ")
+password = input("Enter your password: ")
+try:
+    if username != user_data['username'] or password != user_data['password']:
+        raise ValueError("Invalid username or password ❌")
+except ValueError as e:
+    print(f"Error: {e}")
+else:
+    print("Login Successful ✅")
