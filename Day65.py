a56d2063aaf947d57e5c04ad89f7e7cd37827e9e
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-05-19 10:23:26 +0530

    Day65 second question

--- Day65.py
+++ Day65.py
@@ -30,3 +30,42 @@ for i in user:
 print(freq)
 
 
+# 🔹 Question 2 – Number Pattern Analyzer
+# Write a Python program to:
+# 👉 Take numbers from user separated by space
+# Example:
+# 10 15 20 33 40 55
+# 👉 Store numbers in list
+# 👉 Print:
+# All even numbers
+# All odd numbers
+# Sum of even numbers
+# Sum of odd numbers
+# Example Output:
+# Even Numbers: [10, 20, 40]
+# Odd Numbers: [15, 33, 55]
+# Even Sum: 70
+# Odd Sum: 103
+# ⚠️ Conditions:
+# ✅ Use loops
+# ❌ Do not use sum()
+# ❌ Do not use list comprehension
+
+numbers = list(map(int,input("Enter the numbers separated by space: ").split()))
+even_numbers = []
+odd_numbers = []
+for i in numbers:
+    if i % 2 == 0:
+        even_numbers.append(i)
+    else:
+        odd_numbers.append(i)
+even_sum = 0
+odd_sum = 0
+for i in even_numbers:
+    even_sum += i
+for i in odd_numbers:
+    odd_sum += i
+print(f"Even Numbers: {even_numbers}")
+print(f"Odd Numbers: {odd_numbers}")
+print(f"Even Sum: {even_sum}")
+print(f"Odd Sum: {odd_sum}")
