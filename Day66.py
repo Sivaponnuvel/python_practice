1bde440734fcdeb7c4bc1ae755f8260600ff8154
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-05-20 11:02:00 +0530

    Day66 second question

--- Day66.py
+++ Day66.py
@@ -22,3 +22,31 @@ print(reverse_word)
 
 
 
+# 🔹 Question 2 – Longest Word Finder
+# Write a Python program to:
+# 👉 Take a sentence from user
+# Example:
+# I am learning FastAPI backend
+# 👉 Find the longest word manually
+# 👉 Print:
+# Longest word
+# Length of longest word
+# Example Output:
+# Longest Word: learning
+# Length: 8
+# ⚠️ Conditions:
+# ❌ Do not use max()
+# ✅ Use loops and conditions only
+
+sentence = input("Enter the sentence: ").split()
+longest_word = ""
+longest_length = 0
+for i in sentence:
+    word_length = 0
+    for j in i:
+        word_length += 1
+    if word_length > longest_length:
+        longest_length = word_length
+        longest_word = i
+print(f"Longest Word: {longest_word}")
+print(f"Length: {longest_length}")
