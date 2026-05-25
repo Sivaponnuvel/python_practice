98b2b3c1c3280478ea8e41cff8e69fa2791d0b61
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-05-25 13:20:45 +0530

    Day71 second question

--- Day71.py
+++ Day71.py
@@ -45,3 +45,29 @@ def profile():
 profile()
 
 
+# 🔹 Question 2 – Result Modify Decorator
+# Write a Python program to:
+# 👉 Create a decorator:
+# double_result(func)
+# 👉 Inside decorator:
+# Multiply returned value by 2
+# 👉 Create function:
+# get_number()
+# 👉 Function should return a number from user input
+# 👉 Apply decorator using:
+# @double_result
+# 👉 Print final result
+# Example Output:
+# Enter number: 10
+# Final Result: 20
+
+def double_result(func):
+    def wrapper():
+        print(f"Final Result: {func() * 2}")
+    return wrapper
+
+@double_result
+def get_number():
+    user = int(input("Enter the number: "))
+    return user
+get_number()
