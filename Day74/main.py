59c4f25a0a0adb995e925340598079ba0f844801
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-05-28 07:42:04 +0530

    Day74 second question

--- Day74/main.py
+++ Day74/main.py
@@ -31,3 +31,58 @@ for i in range(len(file1)):
         print(f"Line {i+1}: Different ❌")
 
 
+# 🔹 Question 2 – Exception Handling Calculator
+# Write a Python program to:
+# 👉 Create a calculator using functions:
+# add(a, b)
+# subtract(a, b)
+# multiply(a, b)
+# divide(a, b)
+# 👉 Take:
+# operation from user
+# two numbers
+# 👉 Handle exceptions:
+# If user enters invalid number:
+# Invalid Number ❌
+# If user divides by zero:
+# Cannot divide by zero ❌
+# If user enters invalid operation:
+# Invalid Operation ❌
+# Example Output:
+# Choose Operation: /
+# Enter a: 10
+# Enter b: 2
+# Result: 5.0
+# OR
+# Choose Operation: /
+# Enter a: 10
+# Enter b: 0
+# Cannot divide by zero ❌
+
+def add(a, b):
+    return a + b
+def subtract(a, b):
+    return a - b
+def multiply(a, b):
+    return a * b
+def divide(a, b):
+    return a/ b
+
+try:
+    a = float(input("Enter the first number: "))
+    b = float(input("Enter the second number: "))
+    operation = input("Choose Operation(+,-,*,/): ")
+    if operation == "+":
+        print(f"Addition: {add(a, b)}")
+    elif operation == "-":
+        print(f"Subtraction: {subtract(a, b)}")
+    elif operation == "*":
+        print(f"Multiplication: {multiply(a, b)}")
+    elif operation == "/":
+        print(f"Division: {divide(a, b)}")
+    else:
+        print("Invalid Operation ❌")
+except ValueError:
+    print("Invalid Number ❌")
+except ZeroDivisionError:
+    print("Cannot divide by zero ❌")
