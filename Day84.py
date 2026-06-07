3746f7a2792f354715e398a88372bc38bd4f10e5
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-06-07 08:24:12 +0530

    Day84 second question

--- Day84.py
+++ Day84.py
@@ -36,3 +36,50 @@ for i in students:
 print(dictionary)
 
 
+# 🔹 Question 2 – Decorator: Function Access Counter
+# Write a Python program to:
+# 👉 Create a decorator:
+# track_access(func)
+# 👉 Every time the decorated function is called:
+# Print:
+# Function Accessed 1 Time(s)
+# then
+# Function Accessed 2 Time(s)
+# and so on...
+# 👉 Create function:
+# show_profile(name)
+# 👉 Function should print:
+# Welcome <name>
+# 👉 Apply decorator using:
+# @track_access
+# 👉 Take name from user
+# 👉 Call function 3 times
+# Example Output
+# Enter Name: Siva
+# Function Accessed 1 Time(s)
+# Welcome Siva
+# Function Accessed 2 Time(s)
+# Welcome Siva
+# Function Accessed 3 Time(s)
+# Welcome Siva
+# ⚠️ Conditions:
+# ✅ Decorator must work with arguments
+# ✅ Use closure variable for count
+# ✅ Do not use global variables
+
+def track_access(func):
+    count = [0]
+    def wrapper(*args, **kwargs):
+        count[0] += 1
+        print(f"Function Accessed {count[0]} Time(s)")
+        func(*args, **kwargs)
+    return wrapper
+
+@track_access
+def show_profile(name):
+    print(f"Welcome {name}")
+
+name = input("Enter Name: ")
+show_profile(name)
+show_profile(name)
+show_profile(name)
