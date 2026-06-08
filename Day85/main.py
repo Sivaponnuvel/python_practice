f8f994803bf8c8105f6fe4af7141944c4c4bf55e
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-06-08 13:38:04 +0530

    Day85 second question

--- Day85/main.py
+++ Day85/main.py
@@ -50,3 +50,87 @@ except InvalidLoginError as e:
     print(e)
 
 
+# 🔹 Question 2 – JSON: Student Marks Manager
+# Write a Python program to:
+# 👉 Create a JSON file:
+# students.json
+# 👉 Store data like:
+# [
+#     {"id": 1, "name": "Siva", "marks": 85},
+#     {"id": 2, "name": "Ram", "marks": 72}
+# ]
+# 👉 Create functions:
+# view_students()
+# add_student(student_id, name, marks)
+# find_topper()
+# Function Details
+# ✅ view_students()
+# Print all students
+# Example:
+# ID: 1
+# Name: Siva
+# Marks: 85
+# ✅ add_student(student_id, name, marks)
+# Add a new student to JSON file
+# Print:
+# Student Added ✅
+# ✅ find_topper()
+# Find student with highest marks manually
+# ❌ Do not use max()
+# Print:
+# Topper:
+# Siva - 85
+# Program Flow
+# Create initial JSON data
+# View students
+# Take new student details from user
+# Add student
+# Find topper
+# ⚠️ Conditions:
+# ✅ Use json.load()
+# ✅ Use json.dump()
+# ✅ Use functions
+# ✅ Use file handling
+# ❌ Do not use max()
+
+import json
+
+file_path = "D:/Backend/Python/Own try/practice/Day85/students.json"
+
+students = [{"id": 1, "name": "Siva", "marks": 85}, {"id": 2, "name": "Ram", "marks": 72}]
+
+with open(file_path, "w") as file:
+    json.dump(students,file)
+
+def view_students():
+    with open(file_path,"r")as file:
+        students = json.load(file)
+        for i in students:
+            print(f"ID: {i['id']}")
+            print(f"Name: {i['name']}")
+            print(f"Marks: {i['marks']}")
+def add_student(student_id, name, marks):
+    with open(file_path,"r")as file:
+        students = json.load(file)
+    students.append({"id": student_id, "name": name, "marks": marks})
+    with open(file_path,"w")as file:
+        json.dump(students, file)
+    print("Student Added ✅")
+def find_topper():
+    with open(file_path,"r")as file:
+        students = json.load(file)
+    topper = students[0]
+    for i in students:
+        if i["marks"] > topper["marks"]:
+            topper = i
+    print("Topper:")
+    print(f"{topper['name']} - {topper['marks']}")
+
+view_students()
+
+student_id = int(input("Enter ID: "))
+name = input("Enter Name: ")
+marks = int(input("Enter Mark: "))
+add_student(student_id, name, marks)
+
+find_topper()
