db02db8120b8003396303632317d7b3c388b13c2
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-06-13 16:17:33 +0530

    Day89 second question

--- Day89.py
+++ Day89.py
@@ -54,3 +54,51 @@ else:
     print("No Matching Student ❌")
 
 
+# 🔹 Question 2 – Display Students Sorted by Age
+# Using table:
+# students
+# Write a Python program to:
+# 👉 Fetch all students
+# 👉 Sort by age in ascending order using MySQL
+# SELECT * FROM students
+# ORDER BY age ASC
+# 👉 Display records
+# Example Output:
+# --- Students Sorted By Age ---
+# ID: 2
+# Name: Ram
+# Age: 20
+# ID: 3
+# Name: Arun
+# Age: 21
+# ID: 1
+# Name: Siva
+# Age: 23
+# After completing ASC:
+# Try:
+# ORDER BY age DESC
+# to see highest age first.
+# ⚠️ Conditions
+# ✅ Use ORDER BY
+# ✅ Use fetchall()
+# ✅ Sorting should happen in MySQL
+# ❌ Don't sort using Python
+
+cursor.execute("select * from students order by age asc")
+detail = cursor.fetchall()
+print("--- Students Sorted By Age ---")
+for i in detail:
+    print(f"ID: {i[0]}")
+    print(f"Name: {i[1]}")
+    print(f"Age: {i[2]}")
+
+cursor.execute("select * from students order by age desc")
+detail1 = cursor.fetchall()
+print("--- Students Sorted By Age (Descending) ---") 
+for i in detail1:
+    print(f"ID: {i[0]}")
+    print(f"Name: {i[1]}")
+    print(f"Age: {i[2]}")
+
+cursor.close()
+con.close()
