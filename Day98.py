0e295fcac6dd5ed4617a106b9284703a75d77171
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-06-22 15:12:03 +0530

    Day98 second question

--- Day98.py
+++ Day98.py
@@ -31,3 +31,45 @@ print(f"Minimum Age: {min_age[0]}")
 print(f"Maximum Age: {max_age[0]}")
 
 
+# Question 2 – Search Students Using IN
+# Using table students, write a Python program to search students whose age is in a given list.
+# Take input
+# Enter Age 1: 20
+# Enter Age 2: 23
+# Enter Age 3: 25
+# Query
+# SELECT * FROM students WHERE age IN (%s, %s, %s)
+# Example Output
+# Matching Students:
+# ID: 1
+# Name: Siva
+# Age: 23
+# ID: 4
+# Name: Vijay
+# Age: 25
+# If no records found
+# No Matching Students ❌
+# ⚠️ Conditions
+# Use IN
+# Use fetchall()
+# Use parameterized query with three placeholders
+# ❌ Don't filter in Python loops
+
+age_1 = int(input("Enter Age 1: "))
+age_2 = int(input("Enter Age 2: "))
+age_3 = int(input("Enter Age 3: "))
+
+cursor.execute("select * from students where age in (%s, %s, %s)",(age_1, age_2, age_3))
+details = cursor.fetchall()
+
+if details:
+    print("Matching Students:")
+    for i in details:
+        print(f"ID: {i[0]}")
+        print(f"Name: {i[1]}")
+        print(f"Age: {i[2]}")
+else:
+    print("No Matching Students ❌")
+
+cursor.close()
+con.close()
