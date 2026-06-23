40112a321fde5a0da0da6afbef94cbd60d2fe586
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-06-23 15:50:48 +0530

    Day99 second question

--- Day99/main.py
+++ Day99/main.py
@@ -54,3 +54,58 @@ else:
     print("Invalid Animal ❌")
 
 
+# 🔹 Question 2 – File Handling: Student Notes System
+# Create a file:
+# notes.txt
+# Program Flow
+# Take 3 notes from user.
+# Example:
+# Enter Note: Learn Python
+# Enter Note: Learn MySQL
+# Enter Note: Build Projects
+# Store all notes into:
+# notes.txt
+# (one note per line)
+# After saving:
+# Read the file and display:
+# --- Notes ---
+# Learn Python
+# Learn MySQL
+# Build Projects
+# Display:
+# Total Notes: 3
+# Example Output
+# Enter Note: Learn Python
+# Enter Note: Learn MySQL
+# Enter Note: Build Projects
+# Notes Saved ✅
+# --- Notes ---
+# Learn Python
+# Learn MySQL
+# Build Projects
+# Total Notes: 3
+# ⚠️ Conditions
+# ✅ Use file handling
+# ✅ Use loop
+# ✅ Use write()
+# ✅ Use readlines()
+# ❌ Don't store notes in JSON
+# ❌ Don't use external libraries
+
+notes_txt = "D:/Backend/Python/Own try/practice/Day99/notes.txt"
+
+with open(notes_txt,"w") as file:
+    for i in range(3):
+        note = input("Enter Note: ")
+        file.write(note + "\n")
+    file.close()
+    print("Notes Saved ✅")
+
+with open(notes_txt, "r") as file:
+    notes = file.readlines()
+    file.close()
+
+print("--- Notes ---")
+for i in notes:
+    print(i.strip())
+print(f"Total Notes: {len(notes)}")
