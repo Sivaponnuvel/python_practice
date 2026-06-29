f90ff9bd855a577b1eb2c064db9cad0d3585772b
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-06-29 14:09:56 +0530

    Day105 second question

--- Day105/main.py
+++ Day105/main.py
@@ -49,3 +49,54 @@ with FileLogger("D:/Backend/Python/Own try/practice/Day105/log.txt")as file:
     print("Message Saved Successfully ✅")
 
 
+# 🔹 Question 2 – OOP: Operator Overloading
+# Create a class:
+# Book
+# Constructor
+# Accept:
+# title
+# pages
+# Overload the + operator.
+# When two Book objects are added:
+# book1 + book2
+# Return the total number of pages.
+# Program Flow
+# Take input for two books.
+# Example:
+# Enter Book 1 Title: Python
+# Enter Book 1 Pages: 350
+# Enter Book 2 Title: MySQL
+# Enter Book 2 Pages: 250
+# Create objects.
+# Add them:
+# total = book1 + book2
+# Display:
+# Total Pages: 600
+# Example Output
+# Enter Book 1 Title: Python
+# Enter Book 1 Pages: 300
+# Enter Book 2 Title: Django
+# Enter Book 2 Pages: 450
+# Total Pages: 750
+# ⚠️ Conditions
+# ✅ Use __add__()
+# ✅ Create two objects
+# ✅ Return the total pages
+# ❌ Don't create a separate function for addition
+# ❌ Don't add page values directly outside the class
+
+class Book:
+    def __init__(self, title, pages):
+        self.title = title
+        self.pages = pages
+    def __add__(self, other):
+        return self.pages + other.pages
+
+title1 = input("Enter Book 1 Title: ")
+pages1 = int(input("Enter Book 1 Pages: "))
+title2 = input("Enter Book 2 Title: ")
+pages2 = int(input("Enter Book 2 Pages: "))
+
+book1 = Book(title1, pages1)
+book2 = Book(title2, pages2)
+print(f"Total Pages: {book1 + book2}")
