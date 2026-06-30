c1052f1bd0d94c449a1b1051ed9c9f17c2b0c86a
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-06-30 14:53:24 +0530

    Day106 second question

--- Day106.py
+++ Day106.py
@@ -49,3 +49,58 @@ else:
     print("No Students Found ❌")
 
 
+# 🔹 Question 2 – Display Students Sorted by Age and Name
+# Using table:
+# students
+# Write a Python program to:
+# 👉 Display all students.
+# Use the following query:
+# SELECT *
+# FROM students
+# ORDER BY age ASC, name ASC;
+# This means:
+# First sort by age (smallest to largest).
+# If two students have the same age, sort them alphabetically by name.
+# Example Table
+# ID	Name	Age
+# 1	Siva	23
+# 2	Ram	21
+# 3	Arun	23
+# 4	Vijay	21
+# Example Output
+# --- Students Sorted ---
+# ID: 2
+# Name: Ram
+# Age: 21
+# ID: 4
+# Name: Vijay
+# Age: 21
+# ID: 3
+# Name: Arun
+# Age: 23
+# ID: 1
+# Name: Siva
+# Age: 23
+# If there are no records:
+# No Students Found ❌
+# ⚠️ Conditions
+# ✅ Use ORDER BY age ASC, name ASC
+# ✅ Use fetchall()
+# ✅ Display using a loop
+# ❌ Don't sort in Python
+# ❌ Don't use sorted()
+
+cursor.execute("select * from students order by age asc, name asc")
+detail = cursor.fetchall()
+
+if detail:
+    print("--- Students Sorted ---")
+    for i in detail:
+        print(f"ID: {i[0]}")
+        print(f"Name: {i[1]}")
+        print(f"Age: {i[2]}")
+else:
+    print("No Students Found ❌")
+
+cursor.close()
+con.close()
