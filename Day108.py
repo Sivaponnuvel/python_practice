cc4268370fb9d80e84c6add3aaebbd3d340c36f3
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-07-02 08:35:17 +0530

    Day108 first question

---
+++ Day108.py
@@ -0,0 +1,57 @@
+# 🔹 Question 1 – Display Students Whose Name Starts With a Given Letter
+# Using table:
+# students
+# Write a Python program to:
+# 👉 Take the first letter of a student's name from the user.
+# Example:
+# Enter Starting Letter: S
+# Use the query:
+# SELECT *
+# FROM students
+# WHERE name LIKE %s;
+# Pass the parameter as:
+# (letter + "%",)
+# Display all matching students.
+# Example Output
+# Enter Starting Letter: S
+# Matching Students:
+# ID: 1
+# Name: Siva
+# Age: 23
+# ID: 5
+# Name: Surya
+# Age: 20
+# If no students are found:
+# No Matching Students ❌
+# ⚠️ Conditions
+# ✅ Use LIKE
+# ✅ Use parameterized query
+# ✅ Use fetchall()
+# ✅ Display using a loop
+# ❌ Don't filter names in Python
+# ❌ Don't use startswith()
+
+import mysql.connector
+
+con = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    database = "student_db",
+    password = "siva2025"
+)
+cursor = con.cursor()
+
+letter = input("Enter Starting Letter: ")
+cursor.execute("select * from students where name like %s",(letter + "%",))
+details = cursor.fetchall()
+
+if details:
+    print("Matching Students:")
+    for i in details:
+        print(f"ID: {i[0]}")
+        print(f"Name: {i[1]}")
+        print(f"Age: {i[2]}")
+else:
+    print("No Matching Students ❌")
+
+
