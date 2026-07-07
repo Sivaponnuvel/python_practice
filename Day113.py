6f7f180548e10fbf6f5034a34f8ae4cc5a0ff123
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-07-07 12:54:25 +0530

    Day113 second question

--- Day113.py
+++ Day113.py
@@ -35,3 +35,63 @@ else:
     print(f"Second Largest Unique Number: {asc[-2]}")
 
 
+# 🔹 Question 2 – OOP: Employee Salary System
+# Create a class:
+# Employee
+# Constructor
+# Accept:
+# name
+# salary
+# Instance Methods
+# display()
+# Output:
+# Name   : Siva
+# Salary : 25000
+# increment(percent)
+# Increase the employee's salary by the given percentage.
+# Formula:
+# new_salary = salary + (salary × percent / 100)
+# Program Flow
+# Take details for one employee.
+# Example:
+# Enter Name: Siva
+# Enter Salary: 25000
+# Enter Increment Percentage: 10
+# Output:
+# Updated Employee Details
+# Name   : Siva
+# Salary : 27500.0
+# Example 2
+# Enter Name: Ram
+# Enter Salary: 18000
+# Enter Increment Percentage: 25
+# Output:
+# Updated Employee Details
+# Name   : Ram
+# Salary : 22500.0
+# ⚠️ Conditions
+# ✅ Use a class
+# ✅ Use constructor
+# ✅ Use two instance methods
+# ✅ Update the object's salary
+# ❌ Don't calculate the increment outside the class
+
+class Employee:
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+    def display(self):
+        print(f"Name   : {self.name}")
+        print(f"Salary : {self.salary}")
+    def increment(self, percentage):
+        self.salary = self.salary + (self.salary * percentage / 100)
+
+name = input("Enter Name: ")
+salary = float(input("Enter Salary: "))
+percentage = float(input("Enter Increment Percentage: "))
+
+emp = Employee(name, salary)
+emp.increment(percentage)
+
+print("Updated Employee Details")
+emp.display()
