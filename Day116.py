86a2a5923f3284b00bb0eb71eb1bf99dfcbed788
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-07-10 07:36:13 +0530

    Day116 second question

--- Day116.py
+++ Day116.py
@@ -42,3 +42,50 @@ else:
     print("Student Not Found ❌")
 
 
+# 🔹 Question 2 – Display Students Between Two Ages
+# Using table:
+# students
+# Write a Python program to:
+# 👉 Take:
+# Enter Minimum Age:
+# Enter Maximum Age:
+# Use the query:
+# SELECT *
+# FROM students
+# WHERE age BETWEEN %s AND %s
+# ORDER BY age ASC;
+# Display:
+# Students Found:
+# ID: 2
+# Name: Ram
+# Age: 21
+# ID: 5
+# Name: Arun
+# Age: 23
+# If no students match:
+# No Matching Students ❌
+# ⚠️ Conditions
+# ✅ Use BETWEEN
+# ✅ Use ORDER BY
+# ✅ Use parameterized query
+# ✅ Use fetchall()
+# ✅ Display using a loop
+# ❌ Don't filter ages in Python.
+# ❌ Don't sort using Python.
+
+min_age = int(input("Enter Minimum Age: "))
+max_age = int(input("Enter Maximum Age: "))
+cursor.execute("select * from students where age between %s and %s order by age asc",(min_age, max_age))
+details = cursor.fetchall()
+
+if details:
+    print("Students Found: ")
+    for i in details:
+        print(f"ID: {i[0]}")
+        print(f"Name: {i[1]}")
+        print(f"Age: {i[2]}")
+else:
+    print("No Matching Students ❌")
+
+cursor.close()
+con.close()
