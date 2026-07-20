ceb20e89bbddc9864fd44b2e34468353160055b9
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-07-20 18:22:42 +0530

    Day126 second question

--- Day126.py
+++ Day126.py
@@ -38,3 +38,31 @@ for i in details:
     print(f"{i[0]} {i[1]} {i[2]}")
 
 
+# 🔹 Question 2 – Display the Oldest Student(s)
+# Write a Python program to display the details of the student(s) having the maximum age.
+# Note: If more than one student has the same maximum age, display all of them.
+# Table: students
+# Column Name	Type
+# id	INT
+# name	VARCHAR
+# age	INT
+# Example
+# Output
+# 2 Rahul 24
+# 5 Vijay 24
+# ⚠️ Conditions
+# ✅ Use only one SQL query
+# ✅ Use MAX(age)
+# ✅ Use fetchall()
+# ✅ Display using a loop
+# ❌ Don't first fetch all records and then find the maximum age in Python
+# ❌ Don't use two SQL queries
+
+cursor.execute("select * from students where age = (select max(age) from students)")
+detail = cursor.fetchall()
+
+for i in detail:
+    print(f"{i[0]} {i[1]} {i[2]}")
+
+cursor.close()
+con.close()
