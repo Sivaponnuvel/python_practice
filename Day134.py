62ad258560f2f4c85b420138e115ddea15b350c6
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-07-28 11:29:34 +0530

    Day134 second question

--- Day134.py
+++ Day134.py
@@ -46,3 +46,52 @@ else:
     print("Student Not Found ❌")
 
 
+# 🔹 Question 2 – Delete Students Younger Than a Given Age
+# Write a Python program to delete all students whose age is less than the age entered by the user.
+# Table: students
+# Column Name	Type
+# id	INT
+# name	VARCHAR
+# age	INT
+# Program Flow
+# Take an age from the user.
+# Delete all students whose age is less than the entered age.
+# If records are deleted, display:
+# Total Students Deleted: <count>
+# Otherwise, display:
+# No Students Deleted ❌
+# Example
+# Input
+# Enter Age: 20
+# Suppose the table contains:
+# id	name	age
+# 1	Siva	18
+# 2	Rahul	21
+# 3	Priya	19
+# 4	Vijay	22
+# After execution:
+# id	name	age
+# 2	Rahul	21
+# 4	Vijay	22
+# Output
+# Total Students Deleted: 2
+# ⚠️ Conditions
+# ✅ Use one DELETE query
+# ✅ Use a parameterized query (%s)
+# ✅ Use commit()
+# ✅ Use cursor.rowcount
+# ❌ Don't delete records in Python
+# ❌ Don't use SELECT before DELETE
+# ❌ Don't use more than one SQL query
+
+age = int(input("Enter Age: "))
+cursor.execute("delete from students where age < %s",(age,))
+con.commit()
+
+if cursor.rowcount > 0:
+    print(f"Total Students Deleted: {cursor.rowcount}")
+else:
+    print("No Students Deleted ❌")
+
+cursor.close()
+con.close()
