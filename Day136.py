2928a80bf2699abeb424ff3a8aaed9251d137e71
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-07-30 09:18:47 +0530

    Day136 second question

--- Day136.py
+++ Day136.py
@@ -46,3 +46,56 @@ if cursor.rowcount > 0:
     print("Student Added Successfully ✅")
 
 
+# 🔹 Question 2 – Insert Multiple Students
+# Write a Python program to insert multiple student records into the students table.
+# Table: students
+# Column Name	Type
+# id	INT
+# name	VARCHAR
+# age	INT
+# Program Flow
+# Ask the user how many students they want to insert.
+# Use a loop to get each student's:
+# Name
+# Age
+# Insert each student into the database.
+# After all insertions are complete, commit the transaction once.
+# Display:
+# Total Students Added: <count>
+# Example
+# Input
+# How Many Students: 2
+# Enter Student 1 Name: Priya
+# Enter Student 1 Age: 20
+# Enter Student 2 Name: Rahul
+# Enter Student 2 Age: 21
+# Output
+# Total Students Added: 2
+# ⚠️ Conditions
+# ✅ Take the number of students from the user
+# ✅ Use a loop
+# ✅ Use a parameterized query (%s)
+# ✅ Use INSERT INTO
+# ✅ Call commit() only once after the loop
+# ✅ Use cursor.rowcount
+# ❌ Don't use executemany()
+# ❌ Don't use more than one INSERT statement inside the loop
+
+n = int(input("How Many Students: "))
+total_students = 0
+
+for i in range(1, n + 1):
+    name1 = input(f"Enter Student {i} Name: ")
+    age1 = int(input(f"Enter Student {i} Age: "))
+
+    cursor.execute("insert into students(name,age) values(%s, %s)",(name1, age1))
+
+    if cursor.rowcount > 0:
+        total_students += cursor.rowcount
+
+con.commit()
+
+print(f"Total Students Added: {total_students}")
+
+cursor.close()
+con.close()
