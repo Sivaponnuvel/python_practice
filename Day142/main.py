93ba43bcb7c544226eb69e3398d3d112f141f04c
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-08-05 01:12:46 +0530

    Day142 second question

--- Day142/main.py
+++ Day142/main.py
@@ -43,3 +43,44 @@ except FileNotFoundError:
     print("File Not Found ❌")
 
 
+# 🔹 Question 2 – Modules (Intermediate): Create and Use Your Own Module
+# This question has 2 files.
+# File 1: calculator.py
+# Create a module named calculator.py.
+# Create the following functions:
+# add(a, b)
+# subtract(a, b)
+# multiply(a, b)
+# Each function should return the result.
+# File 2: main.py
+# Program Flow
+# Import the calculator module.
+# Take two integers from the user.
+# Display:
+# Addition
+# Subtraction
+# Multiplication
+# Example
+# Input
+# Enter First Number: 10
+# Enter Second Number: 5
+# Output
+# Addition       : 15
+# Subtraction    : 5
+# Multiplication : 50
+# ⚠️ Conditions
+# ✅ Create your own module (calculator.py)
+# ✅ Import the module into main.py
+# ✅ Use functions from the module
+# ✅ Take input from the user
+# ❌ Don't write all functions inside main.py
+# ❌ Don't import any built-in math modules
+
+from calculator import add, subtract, multiply
+
+num1 = int(input("Enter First Number: "))
+num2 = int(input("Enter Second Number: "))
+
+print(f"Addition       : {add(num1, num2)}")
+print(f"Subtraction    : {subtract(num1, num2)}")
+print(f"Multiplication : {multiply(num1, num2)}")
