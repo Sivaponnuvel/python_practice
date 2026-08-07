8e5858a8c6588f39c22844bf0d6c078c6899191c
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-08-07 14:00:24 +0530

    Day144 second question

--- Day144.py
+++ Day144.py
@@ -38,3 +38,39 @@ else:
     print("No Common Elements ❌")
 
 
+# 🔹 Question 2 – Decorators: Display Messages Before and After a Function
+# Write a Python program to create a decorator that displays a message before and after executing a function.
+# Program Flow
+# Create a decorator named display_message.
+# Before calling the original function, display:
+# Function Started
+# After calling the original function, display:
+# Function Ended
+# Create a function named greet() that displays:
+# Welcome to Python
+# Apply the decorator using @display_message.
+# Call the greet() function.
+# Expected Output
+# Function Started
+# Welcome to Python
+# Function Ended
+# ⚠️ Conditions
+# ✅ Create a decorator function
+# ✅ Use an inner function (wrapper)
+# ✅ Use @display_message
+# ✅ Call the original function inside the wrapper
+# ❌ Don't call greet() inside the decorator
+# ❌ Don't modify the greet() function except by using the decorator
+
+def display_message(func):
+    def wrapper():
+        print("Function Started")
+        func()
+        print("Function Ended")
+    return wrapper
+
+@display_message
+def greet():
+    print("Welcome to Python")
+
+greet()
