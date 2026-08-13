48b8691ecfa20f66b95dd22492132038b363d9b1
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-08-13 15:04:19 +0530

    Day150 first question

---
+++ Day150.py
@@ -0,0 +1,55 @@
+# 🔹 Question 1 – MySQL: Display Students Above Average Age
+# Write a Python program to display all students whose age is greater than the average age of all students.
+# Table: students
+# Column	Type
+# id	INT
+# name	VARCHAR
+# age	INT
+# Example Data
+# id   name    age
+# 1    Siva    21
+# 2    Rahul   25
+# 3    Priya   20
+# 4    Arun    28
+# 5    Vijay   22
+# Average age:
+# 23.2
+# Expected Output
+# Students Above Average Age:
+# Rahul : 25
+# Arun  : 28
+# ⚠️ Conditions
+# ✅ Use mysql.connector
+# ✅ Take database connection
+# ✅ Use one SQL query only
+# ✅ Use a subquery with AVG()
+# ✅ Use fetchall()
+# ✅ Display using a loop
+# ❌ Don't calculate average in Python
+# ❌ Don't use a separate SELECT AVG() query
+# ❌ Don't filter the result in Python
+# 💡 Hint
+# Think about:
+# SELECT ...
+# FROM students
+# WHERE age > (SELECT AVG(age) FROM students)
+
+import mysql.connector
+
+con =  mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    database = "student_db",
+    password = "siva2025"
+)
+cursor = con.cursor()
+
+cursor.execute("select * from students where age > (select avg(age) from students)")
+details = cursor.fetchall()
+
+if details:
+    print("Students Above Average Age:")
+    for i in details:
+        print(f"{i[1]} : {i[2]}")
+
+
