4e822cf3d0b365b09010fb714c4536caa7617346
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-08-13 15:05:02 +0530

    Day150 second question

--- Day150.py
+++ Day150.py
@@ -53,3 +53,43 @@ if details:
         print(f"{i[1]} : {i[2]}")
 
 
+# 🔹 Question 2 – MySQL Interview: Second Highest Age
+# Use your existing students table:
+# Column	Type
+# id	INT
+# name	VARCHAR
+# age	INT
+# Example
+# id   name    age
+# 1    Siva    21
+# 2    Rahul   25
+# 3    Priya   20
+# 4    Arun    28
+# 5    Vijay   25
+# Expected Output
+# Second Highest Age: 25
+# If there is no second highest age:
+# Second Highest Age: None
+# ⚠️ Conditions
+# ✅ Use one SQL query only
+# ✅ Use MAX() with a subquery
+# ✅ Use fetchone()
+# ❌ Don't fetch all ages and calculate in Python
+# ❌ Don't use ORDER BY
+# ❌ Don't use more than one SQL query
+# 💡 Hint
+# The SQL logic is:
+# SELECT MAX(age)
+# FROM students
+# WHERE age < (SELECT MAX(age) FROM students)
+
+cursor.execute("select max(age) from students where age < (select max(age) from students)")
+detail = cursor.fetchone()
+
+if detail:
+    print(f"Second Highest Age: {detail[0]}")
+else:
+    print("Second Highest Age: None")
+
+cursor.close()
+con.close()
