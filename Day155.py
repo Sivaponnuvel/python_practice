7510e48fae17bd297df381bd60902ce0f619ef60
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-08-18 17:01:14 +0530

    Day155 second question

--- Day155.py
+++ Day155.py
@@ -47,3 +47,61 @@ print(f"Vowels     : {vowels_count}")
 print(f"Consonants : {consonants_count}")
 
 
+# 🔹 Question 2 – String Interview: First Non-Repeating Character
+# Write a Python program to find the first character that appears only once in a string.
+# Example 1
+# Input:
+# Enter String: programmin
+# Output:
+# First Non-Repeating Character : p
+# Example 2
+# Input:
+# Enter String: aabbcdd
+# Output:
+# First Non-Repeating Character : c
+# Example 3
+# Input:
+# Enter String: aabbcc
+# Output:
+# No Non-Repeating Character ❌
+# Program Flow
+# Take a string from the user.
+# Count the frequency of every character using a dictionary.
+# Traverse the string again.
+# Find the first character whose frequency is 1.
+# Display that character.
+# ⚠️ Conditions
+# ✅ Take input from the user
+# ✅ Use a dictionary
+# ✅ Use loops
+# ✅ Use string methods where appropriate
+# ✅ Preserve original order
+# ❌ Don't use Counter
+# ❌ Don't use set() as the main solution
+# ❌ Don't sort the string
+# ❌ Don't use index() to solve the problem
+# ❌ Don't import any libraries
+# 💡 Hint
+# First create something like:
+# freq = {}
+
+word = input("Enter String: ")
+
+freq = {}
+
+for i in word:
+    if i in freq:
+        freq[i] += 1
+    else:
+        freq[i] = 1
+
+found = False
+
+for i in word:
+    if freq[i] == 1:
+        print(f"First Non-Repeating Character : {i}")
+        found = True
+        break
+
+if not found:
+    print("No Non-Repeating Character ❌")
