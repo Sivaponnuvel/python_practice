10d636b0a97bdf6ff2ff87a6f604a7113c1d5fa4
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-08-23 14:50:19 +0530

    Day160 second question

--- Day160.py
+++ Day160.py
@@ -50,3 +50,57 @@ except ValueError:
     print("Error: Invalid input ❌")
 
 
+# 🔹 Question 2 – Dictionary Interview: Find Duplicate Elements and Their Count
+# Write a Python program to find all duplicate numbers in a list and display their frequency.
+# Program Flow
+# Take space-separated integers from the user.
+# Store them in a list.
+# Use a dictionary to count the frequency of each number.
+# Display only the numbers that appear more than once.
+# Preserve the order of first occurrence.
+# Example 1
+# Input:
+# Enter Numbers: 10 20 30 10 40 20 10
+# Output:
+# Duplicate Elements:
+# 10 : 3
+# 20 : 2
+# Example 2
+# Input:
+# Enter Numbers: 1 2 3 4 5
+# Output:
+# No Duplicate Elements ❌
+# ⚠️ Conditions
+# ✅ Take input from the user
+# ✅ Use a list
+# ✅ Use a dictionary
+# ✅ Use loops
+# ✅ Preserve first occurrence order
+# ❌ Don't use set()
+# ❌ Don't use collections.Counter
+# ❌ Don't sort the list
+# ❌ Don't import any libraries
+# 💡 Hint:
+# freq = {}
+# First count all elements, then loop through the dictionary and display values where:
+# count > 1
+
+numbers = list(map(int, input("Enter Numbers: ").split()))
+freq = {}
+for i in numbers:
+    if i in freq:
+        freq[i] += 1
+    else:
+        freq[i] = 1
+
+found = False
+
+for number, count in freq.items():
+    if count > 1 :
+        if not found:
+            print("Duplicate Elements:")
+        print(f"{number} : {count}")
+        found = True
+
+if not found:
+    print("No Duplicate Elements ❌")
