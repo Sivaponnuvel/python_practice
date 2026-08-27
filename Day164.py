c5e8cd8b49ad3cc901279d8f3da4af53639d6cd6
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-08-27 09:58:13 +0530

    Day164 second question

--- Day164.py
+++ Day164.py
@@ -31,3 +31,28 @@ for key, value in students.items():
         print(f"{key} {value}")
 
 
+# 🔹 Question 2 – Function + List
+# Create a function:
+# find_even_numbers(numbers)
+# The function should accept a list of numbers and return a new list containing only the even numbers.
+# Example:
+# numbers = [10, 15, 22, 31, 40, 55, 68]
+# print(find_even_numbers(numbers))
+# Expected Output:
+# [10, 22, 40, 68]
+# ⚠️ Conditions:
+# Use a function
+# Use a for loop
+# Use %
+# Return the result
+# Don't use filter()
+
+def find_even_numbers(number):
+    even_numbers = []
+    for i in number:
+        if i % 2 == 0:
+            even_numbers.append(i)
+    return even_numbers
+
+numbers = [10, 15, 22, 31, 40, 55, 68]
+print(find_even_numbers(numbers))
