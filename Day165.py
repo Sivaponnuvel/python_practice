50940bc57bd0b0bd604027dac727b44291c2e197
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-08-28 10:55:32 +0530

    Day165 second question

--- Day165.py
+++ Day165.py
@@ -25,3 +25,28 @@ text = "Python Programming"
 print(count_vowels(text))
 
 
+# 🔹 Question 2 – List + Function
+# Create a function:
+# find_largest(numbers)
+# The function should accept a list of numbers and return the largest number without using max().
+# Example:
+# numbers = [45, 12, 89, 34, 67]
+# print(find_largest(numbers))
+# Expected Output:
+# 89
+# ⚠️ Conditions:
+# Use a function
+# Use a for loop
+# Use an if condition
+# ❌ Don't use max()
+# Return the largest number
+
+def find_largest(numbers):
+    max_num = numbers[0]
+    for i in numbers:
+        if i > max_num:
+            max_num = i
+    return max_num
+
+numbers = [45, 12, 89, 34, 67]
+print(find_largest(numbers))
