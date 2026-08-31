d6ba85a02e2552de56bb1b130a529dbc5634c0e8
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-08-31 09:48:39 +0530

    Day168 second question

--- Day168/main.py
+++ Day168/main.py
@@ -44,3 +44,44 @@ with open("D:/Backend/Python/Own try/python_practice/Day168/students.txt")as fil
     print(f"Total Students: {len(students)}")
 
 
+# 🔹 Question 2 – File Handling: Word Counter
+# Write a Python program that creates a file named:
+# message.txt
+# Take a sentence from the user and write it into the file.
+# Then read the file and calculate:
+# Total number of words
+# Total number of characters
+# Number of vowels
+# Example Input
+# Enter a sentence: Python is easy to learn
+# Expected Output
+# Words: 5
+# Characters: 23
+# Vowels: 8
+# ⚠️ Conditions
+# ✅ Use input()
+# ✅ Use with open()
+# ✅ Use write()
+# ✅ Read the file after writing
+# ✅ Use .split() to count words
+# ✅ Use a for loop to count vowels
+# ✅ Use an if condition
+# ❌ Don't use external libraries
+# ❌ Don't hardcode the sentence
+
+sentence = input("Enter a sentence: ")
+
+with open("D:/Backend/Python/Own try/python_practice/Day168/message.txt", "w") as file:
+    file.write(sentence)
+
+with open("D:/Backend/Python/Own try/python_practice/Day168/message.txt") as file:
+    text = file.read()
+    words = text.split()
+    vowels_count = 0
+    for i in text:
+        if i in "aeiouAEIOU":
+            vowels_count += 1
+
+    print(f"Words: {len(words)}")
+    print(f"Characters: {len(text)}")
+    print(f"Vowels: {vowels_count}")
