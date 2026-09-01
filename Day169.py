03bcb7b9c92dcae58714cbfd249063d481d608fb
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-09-01 10:31:05 +0530

    Day169 second question

--- Day169.py
+++ Day169.py
@@ -45,3 +45,46 @@ number = int(input("Enter a number: "))
 print(is_prime(number))
 
 
+# 🔹 Question 2 – Sum of Digits
+# Write a Python program to find the sum of all digits in a given integer.
+# Program Flow
+# Take a number from the user and calculate the sum of its digits.
+# Example 1
+# Input:
+# Enter a number: 12345
+# Output:
+# Sum of digits: 15
+# Because:
+# 1 + 2 + 3 + 4 + 5 = 15
+# Example 2
+# Input:
+# Enter a number: 908
+# Output:
+# Sum of digits: 17
+# Because:
+# 9 + 0 + 8 = 17
+# ⚠️ Conditions
+# ✅ Use input()
+# ✅ Convert input to int
+# ✅ Use a while loop
+# ✅ Use % operator
+# ✅ Use // operator
+# ❌ Don't convert the number into a string
+# ❌ Don't use sum()
+# ❌ Don't use any libraries
+# 💡 Hint
+# For a number like 123:
+# 123 % 10 → 3
+# 123 // 10 → 12
+# Then repeat until the number becomes 0.
+
+num = int(input("Enter a Number: "))
+
+sum_number = 0
+
+while num > 0:
+    digit = num % 10
+    sum_number += digit
+    num //= 10
+
+print(f"Sum of digits: {sum_number}")
