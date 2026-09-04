a847da5e8c4014c5f5bb1de459b75377f06c6934
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-09-05 05:07:25 +0530

    Day173 second question

--- Day173.py
+++ Day173.py
@@ -76,3 +76,55 @@ obj = Student(name, marks)
 obj.display_result()
 
 
+# 🔹 Question 2 – Decorator: Check Positive Number
+# Write a Python program using a decorator to check whether the number passed to a function is positive or not.
+# Create a decorator:
+# check_positive
+# Apply it to:
+# display_number(number)
+# Program Flow
+# Take a number from the user.
+# If the number is positive, the decorator should allow the function to execute.
+# Example 1:
+# Enter a number: 25
+# Output:
+# Positive number ✅
+# Number: 25
+# If the number is zero or negative:
+# Example 2:
+# Enter a number: -10
+# Output:
+# Number must be positive ❌
+# ⚠️ Conditions
+# ✅ Create a decorator function
+# ✅ Create a wrapper function
+# ✅ Use @check_positive
+# ✅ Use *args
+# ✅ Call the original function only when the number is positive
+# ✅ Use input()
+# ❌ Don't put the positive-number checking logic inside display_number()
+# ❌ Don't use any libraries
+# 💡 Hint
+# Your decorator should follow this general idea:
+# def check_positive(func):
+#     def wrapper(*args):
+#         # check the number
+#         # call func() if valid
+#     return wrapper
+
+def check_positive(func):
+    def wrapper(*args):
+        number = args[0]
+        if number > 0:
+            print("Positive number ✅")
+            return func(*args)
+        else:
+            print("Number must be positive ❌")
+    return wrapper
+
+@check_positive
+def display_number(number):
+    print(f"Number: {number}")
+
+number = int(input("Enter a number: "))
+display_number(number)
