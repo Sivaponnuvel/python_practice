2fff218c0f13d9f5eb32f23ef78bf969a1d74f8a
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-09-12 10:33:44 +0530

    Day180 second question

--- Day180.py
+++ Day180.py
@@ -20,3 +20,39 @@ number = int(input("Enter a number: "))
 print(f"Square of {number}: {result(number)}")
 
 
+# 🔹 Question 2 – Prime Number
+# Write a Python program to check whether a given number is a prime number.
+# A prime number has exactly two factors: 1 and itself.
+# Examples:
+# 2 → Prime
+# 7 → Prime
+# 9 → Not Prime
+# Program Flow
+# Input:
+# Enter a number: 17
+# Expected Output:
+# 17 is a Prime Number ✅
+# For a non-prime:
+# Input:
+# Enter a number: 15
+# Expected Output:
+# 15 is Not a Prime Number ❌
+# ⚠️ Conditions
+# ✅ Use input()
+# ✅ Convert input into an integer
+# ✅ Use a loop
+# ✅ Check whether the number has any divisor other than 1 and itself
+# ❌ Don't use libraries
+# ❌ Don't use a predefined number
+# ❌ Don't use a ready-made prime-number function
+
+def is_prime(number):
+    if number <= 1:
+        return f"{number} is Not a Prime Number ❌"
+    for i in range(2, number):
+        if number % i == 0:
+            return f"{number} is Not a Prime Number ❌"
+    return f"{number} is a Prime Number ✅"
+
+num = int(input("Enter a number: "))
+print(is_prime(num))
