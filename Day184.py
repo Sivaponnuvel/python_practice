771f9e8efdc504866d8582bee0f99adc52961d30
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-09-16 12:20:06 +0530

    Day184 second question

--- Day184.py
+++ Day184.py
@@ -25,3 +25,21 @@ string = input("Enter a string: ")
 print(f"Total vowels: {countVowel(string)}")
 
 
+# 🔹 Question 2 – Remove Duplicates from a List
+# Write a Python program to remove duplicate values from a list while maintaining the original order.
+# Example Input:
+# Enter numbers: 10 20 10 30 20 40 30
+# Expected Output:
+# List after removing duplicates: [10, 20, 30, 40]
+# Conditions:
+# Get the numbers from the user.
+# Remove duplicate values.
+# Maintain the original order.
+# Do not simply use set() for the solution.
+
+numbers = list(map(int, input("Enter numbers: ").split()))
+nums = []
+for i in numbers:
+    if i not in nums:
+        nums.append(i)
+print(f"List after removing duplicates: {nums}")
