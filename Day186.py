8d3936cc5c8ff128103d1b64f6b3e852907df4b8
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-09-18 19:12:55 +0530

    Day186 second question

--- Day186.py
+++ Day186.py
@@ -30,3 +30,42 @@ for i in numbers:
 print(f"Second largest number: {second}")
 
 
+# 🔹 Question 2 – Exception Handling
+# Create a Python program for a simple division calculator.
+# The user should enter two numbers and the program should divide the first number by the second number.
+# Example Input:
+# Enter first number: 20
+# Enter second number: 5
+# Expected Output:
+# Result: 4.0
+# Conditions:
+# Use try and except.
+# Handle division by zero.
+# Handle invalid input such as entering "abc" instead of a number.
+# Display user-friendly error messages.
+# The program should not crash when an exception occurs.
+# Example 2:
+# Enter first number: 20
+# Enter second number: 0
+# Expected:
+# Cannot divide by zero ❌
+# Example 3:
+# Enter first number: abc
+# Expected:
+# Invalid input ❌
+# Please enter numbers only.
+
+try:
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+
+    divided = num1 / num2
+
+    print(f"Result: {divided}")
+
+except ValueError:
+    print("Invalid input ❌")
+    print("Please enter numbers only.")
+
+except ZeroDivisionError:
+    print("Cannot divide by zero ❌")
