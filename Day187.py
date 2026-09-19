3ea3a27b2644b65172df5b8dd996a6e514cf45ba
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-09-19 09:40:32 +0530

    Day187 second question

--- Day187.py
+++ Day187.py
@@ -27,3 +27,36 @@ number = int(input("Enter a number: "))
 print(f"Factorial of {number}: {factorial(number)}")
 
 
+# 🔹 Question 2 – Factorial of Each Digit
+# Write a Python program to find the factorial of each digit in a given number.
+# Example Input:
+# Enter a number: 234
+# Expected Output:
+# Factorial of 2: 2
+# Factorial of 3: 6
+# Factorial of 4: 24
+# Conditions:
+# Get a number from the user.
+# Process each digit separately.
+# Create a function factorial() to calculate the factorial of a digit.
+# Use a loop to calculate the factorial.
+# Do not use math.factorial().
+# Do not treat 234 as one number for factorial.
+# Another Example:
+# Enter a number: 521
+# Expected:
+# Factorial of 5: 120
+# Factorial of 2: 2
+# Factorial of 1: 1
+
+def fact(num):
+    fact = 1
+    for i in range(1, num + 1):
+        fact *= i
+    return fact
+
+num = input("Enter a number: ")
+
+for i in num:
+    a = int(i)
+    print(f"Factorial of {a}: {fact(a)}")
