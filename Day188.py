3e34441b3d8fb9ed4b85249d239c9f94c22d59e0
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-09-20 14:35:45 +0530

    Day188 second question

--- Day188.py
+++ Day188.py
@@ -48,3 +48,55 @@ obj.deposit(amount)
 print(f"Updated Balance: {obj.get_balance()}")
 
 
+# 🔹 Question 2 – Inheritance
+# Create a simple Employee → Developer inheritance program.
+# Requirements:
+# Create a parent class:
+# Employee
+# with:
+# name
+# salary
+# display_employee() method
+# Create a child class:
+# Developer
+# with:
+# programming_language
+# The Developer class should inherit the properties and method from Employee.
+# Example Input:
+# Enter name: Siva
+# Enter salary: 30000
+# Enter programming language: Python
+# Expected Output:
+# Employee Name: Siva
+# Salary: 30000
+# Programming Language: Python
+# Conditions:
+# Use inheritance.
+# Use super().__init__() in the child class.
+# The child class should use the parent's display_employee() method.
+# Add a separate method to display the programming language.
+
+class Employee:
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+
+    def display_employee(self):
+        print(f"Employee Name: {self.name}")
+        print(f"Salary: {self.salary}")
+
+class Developer(Employee):
+    def __init__(self, name, salary, language):
+        super().__init__(name, salary)
+        self.language = language
+
+    def display_language(self):
+        print(f"Programming Language: {self.language}")
+    
+name = input("Enter name: ")
+salary = int(input("Enter salary: "))
+language = input("Enter programming language: ")
+emp = Developer(name, salary, language)
+
+emp.display_employee()
+emp.display_language()
