75cec864776f25a1b3f786d18fae244ed697247b
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-09-20 14:35:14 +0530

    Day188 first question

---
+++ Day188.py
@@ -0,0 +1,50 @@
+# 🔹 Question 1 – Encapsulation
+# Create a BankAccount class that demonstrates encapsulation.
+# Requirements:
+# Create a class BankAccount.
+# Store:
+# account_holder
+# balance
+# Make balance a private variable using __balance.
+# Create a method deposit(amount) to add money.
+# Create a method get_balance() to return the current balance.
+# The balance should not be directly accessible outside the class.
+# Example Input:
+# Enter account holder: Siva
+# Enter deposit amount: 5000
+# Expected Output:
+# Account Holder: Siva
+# Balance: 5000
+# Then:
+# Enter deposit amount: 2000
+# Expected:
+# Updated Balance: 7000
+# Conditions:
+# Use __balance.
+# Use methods to access/update the balance.
+# Do not directly modify __balance outside the class.
+
+class BankAccount:
+    def __init__(self, account_holder, balance):
+        self.account_holder = account_holder
+        self.__balance = balance
+
+    def deposit(self, amount):
+        self.__balance += amount
+
+    def get_balance(self):
+        return self.__balance
+
+account_holder = input("Enter account holder: ")
+balance = int(input("Enter deposit amount: "))
+obj = BankAccount(account_holder, balance)
+
+print(f"Account Holder: {obj.account_holder}")
+print(f"Balance: {obj.get_balance()}")
+
+amount = int(input("Enter deposit amount: "))
+obj.deposit(amount)
+
+print(f"Updated Balance: {obj.get_balance()}")
+
+
