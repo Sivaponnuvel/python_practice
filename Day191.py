a564521e5d2e57cef8cd253a7fb77f4d115988f9
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-09-23 13:08:14 +0530

    Day191 second question

--- Day191.py
+++ Day191.py
@@ -40,3 +40,44 @@ username = input("Enter username: ")
 dashboard(username)
 
 
+# 🔹 Question 2 – OOP: Polymorphism
+# Create two classes:
+# Dog
+# Cat
+# Both classes should have the same method:
+# sound()
+# Requirements:
+# Dog should return:
+# Dog says: Bark
+# Cat should return:
+# Cat says: Meow
+# Then create objects of both classes and call the same method:
+# dog.sound()
+# cat.sound()
+# Expected Output:
+# Dog says: Bark
+# Cat says: Meow
+# Conditions:
+# Create two separate classes.
+# Both classes must have a sound() method.
+# Do not use inheritance.
+# Use the same method name with different implementations.
+# This should demonstrate polymorphism.
+
+class Dog:
+    def __init__(self):
+        pass
+    def sound(self):
+        return "Bark"
+
+class Cat:
+    def __init__(self):
+        pass
+    def sound(self):
+        return "Meow"
+
+dog = Dog()
+cat = Cat()
+
+print(f"Dog says: {dog.sound()}")
+print(f"Cat says: {cat.sound()}")
